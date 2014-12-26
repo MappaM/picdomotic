b8791179c94f161c570bb57622e45067ad934a29
Tom Barbette <tom.barbette@ulg.ac.be>
2014-12-26 19:44:17 +0100

    Comments and readability

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
+/**
+ * @author Tom Barbette <tom.barbette@ulg.ac.be>
+ * www.tombarbette.be
+ * GPLv2
+ */
+
 #include <xc.h>
 #include <plib/adc.h>
 #include <plib/usart.h>
 #include <stdlib.h>
 
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
+//Configuration options
 
 // CONFIG1H
 #pragma config FOSC = INTIO67   // Oscillator Selection bits (Internal oscillator block, port function on RA6 and RA7)
@@ -57,16 +62,31 @@
 #pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0001FFh) not protected from table reads executed in other blocks)
 #define _XTAL_FREQ 1000000
 
+
+//Ports assignements
 #define LED PORTAbits.RA2
 #define BTN PORTBbits.RB3
 
+//Relays. Just for info, theses defines are not used
+#define RELAY_0 PORTCbits.RC0
+#define RELAY_1 PORTCbits.RC1
+#define RELAY_2 PORTCbits.RC2
+#define RELAY_3 PORTCbits.RC3
+#define RELAY_4 PORTBbits.RB0
+#define RELAY_5 PORTBbits.RA7
+#define RELAY_6 PORTBbits.RA6
+#define RELAY_7 PORTBbits.RA5
+#define RELAY_8 PORTBbits.RA4
+
 void setMask(unsigned short data);
 
-unsigned short globdata;
-unsigned short globdata2;
-unsigned short globumask;
+unsigned short globdata; //Actual bitmask for the relay. A bit set to one enables the relay
+unsigned short globdata2; //A second bitmask to set if the user do a long press on the button (usually to disable all equipment, but it's usefull if you want to keep a security camera on for example)
+unsigned short globumask; //Bitmask to invert some bits. For example some relay use PNP transistors, so setting the output to 1 will in fact disable the relay...
 
-
+/**
+ * Read default values from EEPROM (can be written using some commands)
+ */
 void setDefault() {
     globdata = (EEPROM_READ(0x00) << 8) | EEPROM_READ(0x01);
     globumask = (EEPROM_READ(0x02) << 8) | EEPROM_READ(0x03) ;
@@ -75,65 +95,93 @@ void setDefault() {
 
 
 /* Do not use : block uart !
+ * Old delay function using the watchdog timer, allowing to do a real SLEEP with less power. But it blocked UART.
+ * As the PIC is throttled to 1MHZ, it didn't gained much power anyway...
+ *
 void __delay_s(int t) {
     WDTCONbits.SWDTEN = 1;
     SLEEP();
     WDTCONbits.SWDTEN = 0;
     CLRWDT();
 }*/
- int chan = 0;
+
+ 
 void main() {
-    OSCCONbits.IRCF2 = 0;     // Set the OSCILLATOR Control Register to 1 MHz
+    
+     // Set the OSCILLATOR Control Register to 1 MHz
+    OSCCONbits.IRCF2 = 0;    
     OSCCONbits.IRCF1 = 1;
     OSCCONbits.IRCF0 = 1;
+    
+    //Set the 2 temperature sensor, the button, the RX UART as input
     TRISC = 0b10000000;
     TRISB = 0b00001000;
     TRISA = 0b00001011;
+    
+    //Enable SBOR, used to reset if voltage drops too much
     SBOREN = 1;
+    
+    //Enable FVR, used to get the fixed value of the FVR (1.2Volts) with the FDC. Usefull as we cannot assume the exactiveness of VDC
     FVREN = 1;
+    
+    //Value for Low Voltage Detection
     HLVDL3 = 1;
     HLVDL2 = 0;
     HLVDL1 = 1;
     HLVDL0 = 0;
+    
+    //Enable voltage detectio to interrupt if voltage drops
     VDIRMAG = 0;
     HLVDEN = 1;
     HLVDIF = 0;
     HLVDIE = 1;
 
+    //Blink if the button is pressed
     while (BTN == 1) {
              __delay_ms(50);
              LED = 1;
              __delay_ms(50);
              LED = 0;
              CLRWDT();
-
     }
+    
+    //Set default values read from eeprom
     setDefault();
 
+    //Get last value saved in EEPROM
     unsigned short lastv = (EEPROM_READ(0x06) << 8) | EEPROM_READ(0x07) ;
     setMask(lastv);
     
+    //Enable UART
     unsigned int spbrg = (unsigned int)( ((float)1 * 1.0e6) /(4.0*(float)9600) + 0.5 - 1.0);
     OpenUSART( USART_TX_INT_OFF & USART_RX_INT_ON & USART_ASYNCH_MODE & USART_EIGHT_BIT &
              USART_CONT_RX & USART_BRGH_HIGH, spbrg );
-    BAUDCONbits.BRG16 = 1;  // needed so we can use a 16-bit spbrg
-        // Note that this is not discussed in the c18 Compiler Libraries guide
+    BAUDCONbits.BRG16 = 1;
+        
+    //Clear the watchdog as we could have already taken some time
     CLRWDT();
-  Delay10KTCYx(1); // small 4x0.0125 s delay to allow communication speed to stabilize
-                    // part of the C18 delays.h library
+    
+    Delay10KTCYx(1); // small 4x0.0125 s delay to allow communication speed to stabilize
 
-CLRWDT();
-  OpenADC(ADC_FOSC_2 & ADC_RIGHT_JUST & ADC_20_TAD, ADC_CH0 & ADC_INT_OFF & ADC_VREFPLUS_VDD & ADC_VREFMINUS_VSS, ADC_0ANA);
-  PIR1bits.ADIF=0;
-  INTCONbits.PEIE=1;
-  PIE1bits.ADIE=0;
+    //Delay?Watchdog. Do it too much is better than resetting...
+    CLRWDT();
+    
+    //Enable ADC
+    OpenADC(ADC_FOSC_2 & ADC_RIGHT_JUST & ADC_20_TAD, ADC_CH0 & ADC_INT_OFF & ADC_VREFPLUS_VDD & ADC_VREFMINUS_VSS, ADC_0ANA);
+    PIR1bits.ADIF=0;
   
-  ei();
+    //Enable interrupt
+    INTCONbits.PEIE=1;
+    PIE1bits.ADIE=0;
+  
+    //Enable the interrupt systems
+    ei();
 
-
+    //Never exit this program...
     while (1) {
         int i = 0;
         
+        //Count the number of milliseconds the user press the button
         while (BTN == 1) {
             i ++;
             __delay_ms(1);
@@ -141,14 +189,15 @@ CLRWDT();
             if (i > 1000)
                 break;
         }
-        if (i > 800) { //Button pressed
+        
+        if (i > 800) { //Long pressed, set the second mode 
             globdata = globdata2;
             setMask(globdata);
             LED = 1;
             for (int i =0; i < 5; i++)
                 __delay_ms(100);
             LED = 0;
-        } else if (i > 5) {
+        } else if (i > 5) { //Small pression, revert to first default mode
             setDefault();
             setMask(globdata);
             LED = 1;
@@ -158,26 +207,36 @@ CLRWDT();
         }
         
         //If users stay on button after action
-         while (BTN == 1 && i > 5) {
+         while (BTN == 1 && i > 5) { //Blink more slowly to let him know it should stop pressing the button.
              __delay_ms(100);
              LED = 1;
              __delay_ms(100);
              LED = 0;
 
-         }
+        }
         CLRWDT();
     }
+    
 }
-  unsigned char ADCStringVal[4];
 
- unsigned short ReadSHORT() {
+
+unsigned char ADCStringVal[4];
+
+/*
+ * Read a short value from UART
+ */
+unsigned short ReadSHORT() {
      unsigned short d = 0;
      d = ReadUSART() << 8;
      while (!DataRdyUSART());
      d |= ReadUSART();
      return d;
- }
+}
 
+/*
+ * Read temperature from an ADC channel connected to LM35
+ * @param channel the channel index
+ */
  float readtemp(int channel) {
      SetChanADC(ADC_CH15);
      ConvertADC();
@@ -193,10 +252,13 @@ CLRWDT();
      return v;
  }
 
+/**
+ *  The interrupt handler. Eaither Low voltage detect or data available from UART
+ */
  void interrupt interruptHandler()
-{
+ {
      if (HLVDIF == 1) {
-
+        //Just blink 5 times...
          for (int i =0 ; i <5; i++) {
              __delay_ms(100);
              LED = 1;
@@ -208,50 +270,50 @@ CLRWDT();
      }
      else if (PIR1bits.RCIF) { //Set mask
         char command = ReadUSART();
-        LED = 1;
+        LED = 1; //Enable led to let know we received the command
         switch (command) {
-            case 'S':
+            case 'S': //Set a bitmask directly received from USART
                 while (!DataRdyUSART());
                 unsigned short device_mask = ReadSHORT();
                 setMask(device_mask);
                 break;
-            case 'G':
+            case 'G': //Get the actual bitmask and write it to the USART
                 while (BusyUSART());
                 char sg[65];
                 utoa(&sg,globdata,2);
                 putsUSART(sg);
                 break;
-            case 'H':
+            case 'H': //idem but give the inverting mask (H is after G on keyboard...)
                 while (BusyUSART());
                 char sg[65];
                 utoa(&sg,globumask,2);
                 putsUSART(sg);
                 break;
-            case 'I':
+            case 'I': //idem but give the default value (I is after H in alphabet. I was sleepy...)
                 while (BusyUSART());
                 unsigned short defaut = (EEPROM_READ(0x00) << 8) | EEPROM_READ(0x01);
                 char sg[65];
                 utoa(&sg,defaut,2);
                 putsUSART(sg);
                 break;
-            case 'J':
+            case 'J': //Idem but gibe the alternative default value (well...)
                 while (BusyUSART());
                 unsigned short defaut = (EEPROM_READ(0x04) << 8) | EEPROM_READ(0x05);
                 char sg[65];
                 utoa(&sg,defaut,2);
                 putsUSART(sg);
                 break;
-            case 'T':
-                 while (BusyUSART());
+            case 'T': //Return Temperature
+                while (BusyUSART());
                 int s;
                 putsUSART(ftoa(readtemp(ADC_CH0),&s)); //convert to string
                 break;
-            case 't':
+            case 't': //Return Temperature of second sensor
                 while (BusyUSART());
                 int s;
-                putsUSART(ftoa(readtemp(ADC_CH1) + 3.0f,&s)); //convert to string
+                putsUSART(ftoa(readtemp(ADC_CH1) + 3.0f,&s)); //We add 3.0f because it's behind a 4m cable... SHould use an AOP but it's simpler to strehten the voltage with a capacitor and calculate the loss due to the resistance of the long cable
                 break;
-            case 'D':
+            case 'D': //Set the Default value
                 while (!DataRdyUSART());
                 char data = ReadUSART();
                 EEPROM_WRITE(0x00,data);
@@ -259,7 +321,7 @@ CLRWDT();
                 data = ReadUSART();
                 EEPROM_WRITE(0x01,data);
                 break;
-            case 'K':
+            case 'K': //set the second default value (don't remember why "K")
                 while (!DataRdyUSART());
                 char data = ReadUSART();
                 EEPROM_WRITE(0x04,data);
@@ -267,7 +329,7 @@ CLRWDT();
                 data = ReadUSART();
                 EEPROM_WRITE(0x05,data);
                 break;
-            case 'U':
+            case 'U': //set the Umask, used to invert the bits for low-enabled device
                 while (!DataRdyUSART());
                 char data = ReadUSART();
                 EEPROM_WRITE(0x02,data);
@@ -277,17 +339,12 @@ CLRWDT();
                 globumask = (EEPROM_READ(0x02) << 8) | EEPROM_READ(0x03) ;
                 setMask(globdata);
                 break;
-            case 'A':
+            case 'A': //enable a device, or multiple at once using added power of two (A for Allumer in french)
                 while (!DataRdyUSART());
                 unsigned short dev = ReadSHORT();
                 setMask(globdata | dev);
                 break;
-           /* case 'P':
-                putsUSART("Sleep...");
-                __delay_ms(1);
-                __delay_s(1);
-                break;*/
-            case 'E':
+            case 'E': //disable a device (E pour Eteindre in french)
                  while (!DataRdyUSART());
                 unsigned short dev = ReadSHORT();
                 setMask(globdata & ~dev);
@@ -296,26 +353,26 @@ CLRWDT();
                 while (BusyUSART());
                 putsUSART("Unknow command");
         }
-        LED = 0;
-        PIR1bits.RCIF = 0;
+        LED = 0; //Disable LED
+        PIR1bits.RCIF = 0; //Clean interrupt
     }
 }
 
 
  void setMask(unsigned short data) {
-
+    //Write the last setted value in EEPROM. If we have a power loss, it will be setted back in the same state. Let's hope it wasn't due to a short circuit...
     EEPROM_WRITE(0x06,data >> 8);
     while (WR);
     EEPROM_WRITE(0x07,data);
 
-     globdata = data;
-     unsigned short out = data ^ globumask;
-     PORTC = (0x0f & out);
-     PORTBbits.RB0 = (out >> 4) & 0x1;
-     PORTAbits.RA7 = (out >> 5) & 0x1;
-     PORTAbits.RA6 = (out >> 6) & 0x1;
-     PORTAbits.RA5 = (out >> 7) & 0x1;
-     PORTAbits.RA4 = (out >> 8) & 0x1;
+    globdata = data;
+    unsigned short out = data ^ globumask;
+    PORTC = (0x0f & out);
+    PORTBbits.RB0 = (out >> 4) & 0x1;
+    PORTAbits.RA7 = (out >> 5) & 0x1; //Stupidly inverted bits for RA4-7... But now I'm stuck with it
+    PORTAbits.RA6 = (out >> 6) & 0x1;
+    PORTAbits.RA5 = (out >> 7) & 0x1;
+    PORTAbits.RA4 = (out >> 8) & 0x1;
  }
 
 
